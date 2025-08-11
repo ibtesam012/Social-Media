ba36aed77c8e4d36f250b36cc65bf69a9354deb9
IBTESAM HUSSAIN SHAH <syedibtesamalishah@gmail.com>
2025-08-11 05:57:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var settingmenu = document.querySelector(".setting-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingMenuToogle() {
+    settingmenu.classList.toggle("setting-menu-height");
+}
+
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+if (localStorage.getItem("theme") === "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+} 
+else if (localStorage.getItem("theme") === "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+} 
+else {
+    localStorage.setItem("theme", "light");
+}
